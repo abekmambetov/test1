ecda861a104b0a167d901ea41aa68028999c92bf
abekmambetov <43209750+abekmambetov@users.noreply.github.com>
2021-08-30 15:07:06 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+
+class StringModifyer{
+	public static function revertCharacters(string $str) : string {  
+
+		for ($i = 0; $i <= mb_strlen($str); $i++) {
+		  $arr[] = mb_substr( $str, $i, 1);
+		}
+		
+        $reversed_arr = array_reverse($arr);
+		
+		$reversed_str = "";
+		for ($i = 0; $i < count($reversed_arr); $i++) {
+		  $reversed_str .= $reversed_arr[$i];
+		}
+		return $reversed_str;
+
+	}
+}
+
+echo StringModifyer::revertCharacters("Привет! Давно не виделись.");

ef8f71320aa9503ca3b8898536151776366662c4
abekmambetov <43209750+abekmambetov@users.noreply.github.com>
2021-09-03 17:13:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
 <?php
 class StringModifyer{
 	public static function revertCharacters(string $str) : string {  
-
+         
+		$special_symbols = ['!', '?', ' ', '-', '*']; 
 		for ($i = 0; $i <= mb_strlen($str); $i++) {
-		  $arr[] = mb_substr( $str, $i, 1);
+		  
+			$symbol = mb_substr( $str, $i, 1);
+
+			if(preg_match('~^\p{Lu}~u', $symbol)) {
+				$upperPos[] = $i;
+			}
+
+			if(in_array($symbol, $special_symbols)) {
+				array_unshift($arr , $symbol);
+				$subStrings[] = array_reverse($arr);
+				$arr = [];
+			} else {
+				$arr[] = mb_substr( $str, $i, 1);
+			}
 		}
-		
-		$reversed_arr = array_reverse($arr);
 		
 		$reversed_str = "";
-		for ($i = 0; $i < count($reversed_arr); $i++) {
-		  $reversed_str .= $reversed_arr[$i];
+
+		for ($i = 0, $i2 = 0; $i < count($subStrings); $i++) {
+
+			for ($i1 = 0; $i1 < count($subStrings[$i]); $i1++) {
+
+				if(in_array($i2, $upperPos)) {
+					$reversed_str .= mb_strtoupper($subStrings[$i][$i1]);
+				} else {
+					$reversed_str .= mb_strtolower($subStrings[$i][$i1]);
+				}
+
+				$i2++;
+			}
+
 		}
+
 		return $reversed_str;
 
 	}
 }
 
-echo StringModifyer::revertCharacters("Привет! Давно не виделись.");
+echo StringModifyer::revertCharacters("ПриВ-ет! Да*вно не виделИсь?");
